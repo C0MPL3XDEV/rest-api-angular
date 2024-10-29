e0c52ebf68c753e12761c67df70501805cb4a07f
C0MPL3XDEV <complexdev3@gmail.com>
2024-10-29 18:39:09 +0100

    Add Registration Page and Navigation Links and Authentication Interceptor
    Introduced a new Register component with accompanying HTML and spec files. Updated the app routes to include the registration path and modified the Login component to provide a link to the registration page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 export const routes: Routes = [
   { path: "login", component: LoginComponent, title: "Log-In Page"},
+  { path: "register", component: RegisterComponent, title: "Register Page" },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
